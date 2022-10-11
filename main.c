475fdde2d0a4831f6d0d78386172b66435a64287
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-11 17:40:03 +0900

    test5

--- main.c
+++ main.c
@@ -2,13 +2,43 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int a){
-	return a*a;
+int combination(int a,int b,int c){
+	int sum=a/(b*c);
+	return sum;
 }
 
+int factorial(int n){
+	int res=1;
+	int i;
+	for(i=1;i<=n;i++){
+		res=res*i;
+	}
+	return res;
+}
+
+
+int get_integer(){
+	int n;
+	printf("숫자를 입력하세요:");
+	scanf("%d",&n);
+	return n;	
+}
+
+
 int main(int argc, char *argv[]) {
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
+	int n;
+	int r;
+	int a;
+	int b;
+	int c;
+	int sum; 
+	n=get_integer();
+	printf("첫번째 수보다 작은 수를 입력하세요\n");
+	r=get_integer();
+	a=factorial(n);
+	b=factorial(n-r);
+	c=factorial(r);
+	sum=combination(a,b,c);
+	printf("%d",sum);
 	return 0;
 }

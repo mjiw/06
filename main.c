57062c8fe35907c46b7747334e9c19c61e846966
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-11 17:01:05 +0900

    test3

--- main.c
+++ main.c
@@ -3,17 +3,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func(int x){
-	
-	printf("func x is at %p\n",&x);
-	
+int sumTwo(int a, int b){
+	return a+b;
 }
 
+int square(int n){
+	return n*n;
+}
+
+int get_max(int x, int y){
+	if(x<y){
+		return y;
+	}
+	else if(x>y){
+		return x;
+	}
+}
 
 int main(int argc, char *argv[]) {
-	int x;
-	printf("main x is at %p\n",&x);
-	func(x);	
+	int res_sum,res_square,res_get_max;
 	
+	res_sum=sumTwo(1,2);
+	res_square=square(2);
+	res_get_max=get_max(1,2);
+	
+	printf("%d %d %d",res_sum,res_square,res_get_max);
 	return 0;
 }
